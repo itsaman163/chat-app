aaf934f76317a62e17aa1f0dbe2cca95c457f6dd
aman kumar <aman163@gmail.com>
2024-08-04 02:20:08 +0530

    discord socket io

--- app.js
+++ app.js
@@ -7,21 +7,50 @@ import cors from "cors";
 import router from "./routes/index.js";
 import bodyParser from "body-parser";
 import multer from "multer";
-import pathErrorHandler from "./middleware/path_handler.js";
+// import pathErrorHandler from "./middleware/path_handler.js";
 import { config } from "./config/index.js";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import userModel from "./models/user.model.js";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server,{
+  connectionStateRecovery: {}
+});
 const form = multer();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(form.array());
 
-
-app.use(cors({ credentials:true, origin:'*'}));
+app.use(cors({ credentials: true, origin: "*" }));
 app.use(cookieParser());
 app.use(express.json());
-app.use('/access',router);
-app.use(pathErrorHandler);
 
-app.listen(config.port, ()=> console.log(`Server running at port ${config.port}`));
+io.on('connection', (socket) => {
+  const discord_name = 'gaming_discord'
+  socket.on(discord_name, async (msg) => {
+    console.log("msg---->>",msg);
+    let result;
+    try {
+      const discord_info = userModel.getDiscordInfo(discord_name);
+      const add_data = {
+        tMessage:msg,
+        iDiscordMasterId:discord_info.discord_master_id,
+        iAdminId:1
+      }
+      await userModel.addUserMessage(add_data);
+    } catch (e) {
+      // TODO handle the failure
+      return;
+    }
+    io.emit(discord_name, msg);
+  });
+});
+app.use("/access", router);
+// app.use(pathErrorHandler);
+
+app.listen(config.port, () =>
+  console.log(`Server running at port ${config.port}`)
+);
--- models/user.model.js
+++ models/user.model.js
@@ -7,6 +7,17 @@ const userModel = {
             .from('moduser')
             .whereRaw('1=1')
         return result;
+    },
+    getDiscordInfo: async(short_code) => {
+        const result = await db
+            .select('dm.iDiscordMasterId as discord_master_id')
+            .from('discord_master as dm')
+            .where("dm.vShortCode",short_code)
+        return result;
+    },
+    addUserMessage: async (add_data) => {
+        let result = await db.insert(add_data).into("discord_message");
+        return result;
     }
 }
 export default userModel;
